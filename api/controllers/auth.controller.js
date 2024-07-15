f34a26736e2e24acc1dbee26acacccb6be620f6f
prishadoshi <prisha.doshi123@gmail.com>
2024-07-15 13:56:36 +0300

    complete sign-up page UI

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,9 +2,10 @@ import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res, next) => {
+  //use next to use the middleware defined in index.js
   const { username, email, password } = req.body;
   const hashedPassword = bcryptjs.hashSync(password, 10); //10- saltnumber
-  const newUser = new User({ username, email, password: hashedPassword });
+  const newUser = new User({ username, email, password: hashedPassword }); //usermodel
   try {
     await newUser.save(); //save in database
     res.status(201).json("User Created successfully"); //201 status which means something is created
